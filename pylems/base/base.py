790de456858874bbfee3dc5eac4f7a887ac41da8
Gautham Ganapathy <gautham@lisphacker.org>
2012-04-23 11:23:46 +0100

    Added pylems/base/base/py

---
+++ pylems/base/base.py
@@ -0,0 +1,10 @@
+"""
+PyLEMS base class.
+
+@author: Gautham Ganapathy
+@organization: Textensor (http://textensor.com)
+@contact: gautham@textensor.com, gautham@lisphacker.org
+"""
+
+class PyLEMSBase(object):
+    pass
