9b4684cf5a91a84a47e1b4edf04bb61c6050d526
Gautham Ganapathy <gautham@lisphacker.org>
2012-04-23 20:52:04 +0100

    Expression parsing in development

--- runlems.py
+++ runlems.py
@@ -9,6 +9,14 @@ import sys
 from pylems.parser.lems import LEMSParser
 from pylems.base.errors import ParseError,ModelError
 
+from pylems.parser.expr import ExprParser
+
+#p = ExprParser('1-(95+.3)*v/(60+40)')
+p = ExprParser('1+2*3-4')
+print p.parse()
+
+sys.exit(0)
+
 model_file = sys.argv[1]
 
 parser = LEMSParser()
