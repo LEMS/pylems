470bcfb657c1772b9fb58ba43a0fecdf0bca22e6
Padraig Gleeson <p.gleeson@gmail.com>
2023-05-11 12:19:28 +0100

    To v0.6.3; make sure case without condition serialises correctly

--- lems/model/dynamics.py
+++ lems/model/dynamics.py
@@ -178,9 +178,10 @@ class Case(LEMSBase):
         """
         Exports this object into a LEMS XML object
         """
-
+        cond = ' condition="{0}"'.format(self.condition) if self.condition is not None else ''
+        print(cond)
         return (
-            '<Case condition="{0}" value="{1}"'.format(self.condition, self.value)
+            '<Case{0} value="{1}"'.format(cond, self.value)
             + "/>"
         )
 
