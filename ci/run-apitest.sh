b2184ce30a79832bed3a1a4ce790a054f546ce52
pgleeson <p.gleeson@gmail.com>
2023-06-16 16:31:58 +0100

    Fix script for masos

--- ci/run-apitest.sh
+++ ci/run-apitest.sh
@@ -12,6 +12,10 @@ python examples/loadtest.py
 
 # Update NeuroML2 path for CI
 if [ "$CI" = "true" ]; then
-    sed -i 's|../NeuroML2|./NeuroML2|g' lems/dlems/exportdlems.py
+    if [ "$RUNNER_OS" = "macOS" ]; then
+        sed -i '' 's|../NeuroML2|./NeuroML2|g' lems/dlems/exportdlems.py;
+    else
+        sed -i 's|../NeuroML2|./NeuroML2|g' lems/dlems/exportdlems.py;
+    fi
 fi
 python lems/dlems/exportdlems.py
